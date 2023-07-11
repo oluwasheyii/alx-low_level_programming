fb28cf806085a2aaf9adb273cd0c07c2ae223a35
oluwasheyii <ferrranmii@gmail.com>
2023-07-11 13:05:11 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,10 +9,8 @@
 int count_word(char *s)
 {
 int flag, c, w;
-
 flag = 0;
 w = 0;
-
 for (c = 0; s[c] != '\0'; c++)
 {
 flag = 0;
@@ -36,13 +34,11 @@ char **strtow(char *str)
 {
 char **matrix, *tmp;
 int i, k = 0, len = 0, words, c = 0, start, end;
-
 while (*(str + len))
 len++;
 words = count_word(str);
 if (words == 0)
 return (NULL);
-
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
 return (NULL);
@@ -67,7 +63,6 @@ c = 0;
 else if (c++ == 0)
 start = i;
 }
-
 matrix[k] = NULL;
 return (matrix);
 }
